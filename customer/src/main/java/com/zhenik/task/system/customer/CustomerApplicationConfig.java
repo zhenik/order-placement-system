4a558ffb6e17307978fcddba5918ef8ac23f03a6
zhenik <nikita.zhevnitskiy@gmail.com>
2019-04-28 13:45:11 +0200

    add swagger doc

--- customer/src/main/java/com/zhenik/task/system/customer/CustomerApplicationConfig.java
+++ customer/src/main/java/com/zhenik/task/system/customer/CustomerApplicationConfig.java
@@ -4,16 +4,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableJpaRepositories(basePackages = ("com.zhenik.task.system.customer.repository"))
 @EntityScan(basePackages = ("com.zhenik.task.system.customer"))
+@EnableSwagger2
 public class CustomerApplicationConfig {
 
   @Bean(name = "OBJECT_MAPPER_BEAN")
@@ -24,4 +29,18 @@ public class CustomerApplicationConfig {
         .build();
   }
 
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.zhenik.task.system.customer.controller"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(
+            new ApiInfoBuilder()
+                .title("API for customer service")
+                .description("Micro-service for running customer management")
+                .version("1.0")
+                .build());
+  }
 }
--- order/src/main/java/com/zhenik/task/system/order/OrderApplicationConfig.java
+++ order/src/main/java/com/zhenik/task/system/order/OrderApplicationConfig.java
@@ -11,10 +11,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableJpaRepositories(basePackages = ("com.zhenik.task.system.order"))
 @EntityScan(basePackages = ("com.zhenik.task.system.order"))
+@EnableSwagger2
 public class OrderApplicationConfig {
 
   @Value(value = "${customer.uri}")
@@ -29,12 +36,27 @@ public class OrderApplicationConfig {
   }
 
   @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder)  {
+  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
     return restTemplateBuilder.build();
   }
 
   @Bean
   public String getBaseCustomerUrl() {
     return baseCustomerUrl;
+  }
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.zhenik.task.system.order.controller"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(
+            new ApiInfoBuilder()
+                .title("API for order service")
+                .description("Micro-service for running order placement management")
+                .version("1.0")
+                .build());
   }
 }
