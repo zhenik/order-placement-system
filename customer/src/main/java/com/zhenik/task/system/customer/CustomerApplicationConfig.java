505cb903b0377720fd4ecfb9e5aa83fe8e5393c0
zhenik <nikita.zhevnitskiy@gmail.com>
2019-04-28 19:23:28 +0200

    add tracing

--- customer/src/main/java/com/zhenik/task/system/customer/CustomerApplicationConfig.java
+++ customer/src/main/java/com/zhenik/task/system/customer/CustomerApplicationConfig.java
@@ -3,9 +3,11 @@ package com.zhenik.task.system.customer;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -42,5 +44,35 @@ public class CustomerApplicationConfig {
                 .description("Micro-service for running customer management")
                 .version("1.0")
                 .build());
+  }
+
+  @Bean
+  @Profile("docker")
+  public io.opentracing.Tracer jaegerTracer() {
+
+    final com.uber.jaeger.Configuration.SamplerConfiguration samplerConfiguration =
+        new com.uber.jaeger.Configuration.SamplerConfiguration(
+            ProbabilisticSampler.TYPE, 1, "jaeger:5778");
+
+    final com.uber.jaeger.Configuration.ReporterConfiguration reporterConfiguration =
+        new com.uber.jaeger.Configuration.ReporterConfiguration(
+            null, "jaeger", 6831, null, null
+        );
+
+    return new com.uber.jaeger.Configuration("customer-service", samplerConfiguration, reporterConfiguration)
+        .getTracer();
+  }
+
+  @Bean
+  @Profile("!docker")
+  public io.opentracing.Tracer jaegerTracerDefault() {
+
+    System.out.println("LOADED DEFAULT CONFIG");
+
+    return new com.uber.jaeger.Configuration(
+        "customer-service",
+        new com.uber.jaeger.Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+        new com.uber.jaeger.Configuration.ReporterConfiguration())
+        .getTracer();
   }
 }
--- order/src/main/java/com/zhenik/task/system/order/OrderApplicationConfig.java
+++ order/src/main/java/com/zhenik/task/system/order/OrderApplicationConfig.java
@@ -3,11 +3,13 @@ package com.zhenik.task.system.order;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
@@ -58,5 +60,31 @@ public class OrderApplicationConfig {
                 .description("Micro-service for running order placement management")
                 .version("1.0")
                 .build());
+  }
+
+  @Bean
+  @Profile("docker")
+  public io.opentracing.Tracer jaegerTracer() {
+    final com.uber.jaeger.Configuration.SamplerConfiguration samplerConfiguration =
+        new com.uber.jaeger.Configuration.SamplerConfiguration(
+            ProbabilisticSampler.TYPE, 1, "jaeger:5778");
+
+    final com.uber.jaeger.Configuration.ReporterConfiguration reporterConfiguration =
+        new com.uber.jaeger.Configuration.ReporterConfiguration(
+            null, "jaeger", 6831, null, null
+        );
+
+    return new com.uber.jaeger.Configuration("order-service", samplerConfiguration, reporterConfiguration)
+        .getTracer();
+  }
+
+  @Bean
+  @Profile("!docker")
+  public io.opentracing.Tracer jaegerTracerDefault() {
+    return new com.uber.jaeger.Configuration(
+            "order-service",
+            new com.uber.jaeger.Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+            new com.uber.jaeger.Configuration.ReporterConfiguration())
+        .getTracer();
   }
 }
